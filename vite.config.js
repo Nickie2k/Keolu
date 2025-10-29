00bec2421a64c26c9725438a298cd395054df8ca
Nickie2k <phihaotran.tech@gmail.com>
2025-10-29 00:32:03 -0500

    Sixth

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Keolu/'
+  base: '/Keolu/',
+  build: {
+    // ... other build options
+    cssMinify: false, // <-- ADD THIS LINE
+  }
 })
